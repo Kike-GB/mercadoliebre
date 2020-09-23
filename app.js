ff7293e2be5a8616e221b9119bf1ee4109208e23
KIKE-GB <enrique.gonzalez.boselli@hotmail.com>
2020-09-22 21:17:04 -0300

    se agrega formulario de registro y login

--- app.js
+++ app.js
@@ -6,17 +6,28 @@ app.get('/', (req, res) => {
     let file = path.resolve('index.html')
     res.sendFile(file);
 });
+app.post('/index.html', (req, res) => {
+    let file = path.resolve('index.html')
+    res.sendFile(file);
+});
+app.get('/registro.html', (req, res) => {
+    let file = path.resolve('registro.html')
+    res.sendFile(file);
+});
+app.get('/login.html', (req, res) => {
+    let file = path.resolve('login.html')
+    res.sendFile(file);
+});
 app.get('*', (req, res) => {
     if (req.url.endsWith('.css')) {
-        let file = path.resolve('style' + req.url);
+        let file = path.resolve('./public/stylesheets' + req.url);
         return res.sendFile(file); 
     }
     let images = ['jpg', 'jpeg', 'gif', 'png', 'svg'];
     let extencion = req.url.split('.')[1];
     if (images.includes(extencion)) {
-        let file = path.resolve('img' + req.url);
+        let file = path.resolve('./public/images' + req.url);
         return res.sendFile(file)
     }
     res.send('File Not Found!!!')
-});
-app.listen (3000, () => console.log('Server On!!!'));
+}).listen(3000, 'localhost', () => console.log('Server ON!!! running in port 3000'));
